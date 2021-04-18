c9dc133eb9c38a9cb3b6f6fd15ff8195bcfd3bba
Chelsie <chensifu4@gmail.com>
2021-04-19 00:27:08 +1000

    CRUD for events

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_055542) do
+ActiveRecord::Schema.define(version: 2021_04_18_140807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "events", force: :cascade do |t|
     t.string "title"
-    t.string "Name"
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
